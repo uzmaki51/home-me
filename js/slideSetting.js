d066f150650ba72d710844012425c842a2667bea
kgs <kgs@gmail.com>
2020-05-23 16:40:18 +0800

    ---

--- js/slideSetting.js
+++ js/slideSetting.js
@@ -5,6 +5,21 @@ $(function() {
         $('#section-top-menu').css('top', (main_height - top_menu_height) + 'px');
     }
 })
+$(window).scroll(function() {
+        var top_menu_height = $('#section-top-menu').height();
+		console.log(top_menu_height);
+        var main_height = window.innerHeight;
+		$('#section-top-menu').css('transition', '0.5s');
+        $('#section-top-menu').css('top', (main_height - top_menu_height) + 'px');
+})
+$( window ).resize(function() {
+    var top_menu_height = $('#section-top-menu').height();
+		console.log(top_menu_height);
+        var main_height = window.innerHeight;
+		$('#section-top-menu').css('transition', '0.5s');
+        $('#section-top-menu').css('top', (main_height - top_menu_height) + 'px');
+});
+
 function showMore() {
     $('.sp-invisible').css('display', 'flex');
 }
